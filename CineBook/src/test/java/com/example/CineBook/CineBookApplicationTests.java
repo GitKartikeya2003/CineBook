456fa9793e9f5f01bf6ca8dc24340bb3e0c05626
Kartikeya <kartikeyayash4@gmail.com>
2026-09-13 16:38:21 +0530

    first commit

---
+++ CineBook/src/test/java/com/example/CineBook/CineBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CineBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
