df8c40b8f6cac1af1dcf2cb6ff65de2c8844038b
Kartikeya <kartikeyayash4@gmail.com>
2026-09-15 19:04:36 +0530

    Wrote a query that gets me the seats that I have to update

--- CineBook/src/main/java/com/example/CineBook/repository/ShowSeatRepository.java
+++ CineBook/src/main/java/com/example/CineBook/repository/ShowSeatRepository.java
@@ -2,12 +2,13 @@ package com.example.CineBook.repository;
 
 import com.example.CineBook.entity.ShowSeat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ShowSeatRepository extends JpaRepository<ShowSeat, Long> {
 
 
-
+    @Query("select s from ShowSeat s where s.show.id=:showId and s.seatLabel in :labels")
     List<ShowSeat> findForUpdate(Long showId, List<String> labels);
 }
